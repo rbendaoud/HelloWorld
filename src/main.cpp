b4f84ea37f21578875af3c732953f34603244aff
Rachid Bendaoud <rbendaoud@free.fr>
2021-06-01 16:59:57 +0200

    new commit 3

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main()
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "That's still ok " << std::endl;
 	std::cout << "new changes" << std::endl;
-	return 0;
+//	return 0;
+//	return 1;
 }
